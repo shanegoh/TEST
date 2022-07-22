ff8fe8c1cd243fe9ac5c5802e90914355d9da781
yipeyayeah <yipeyayeah@gmail.com>
2022-07-22 16:47:58 +0800

    editted bug

--- backend/repo/repo.js
+++ backend/repo/repo.js
@@ -3,64 +3,64 @@ const db = require("../models/index.js");
 
 const User = db.user;
 const Wallet = db.wallet;
-const Transaction = db.transaction
-const Currency = db.currency
-const ExchangeRate = db.exchangeRate
+const Transaction = db.transaction;
+const Currency = db.currency;
+const ExchangeRate = db.exchangeRate;
 
 const findUserByUsername = (username) => {
-    return User.findOne({ where: { username: username } });
+  return User.findOne({ where: { username: username } });
 };
 
 const findCurrencyFromWalletId = (id) => {
-    return Currency.findAll({ where: { wallet_id: 2 } })
-}
+  return Currency.findAll({ where: { wallet_id: id } });
+};
 
 const findAllWalletById = (id) => {
-    return Wallet.findAll({ where: { user_id: id } });
+  return Wallet.findAll({ where: { user_id: id } });
 };
 
 const findExchangeRateByCountry = (exchangeCurrency) => {
-    return ExchangeRate.findAll({
-        where: { exchange_currency: exchangeCurrency },
-    });
+  return ExchangeRate.findAll({
+    where: { exchange_currency: exchangeCurrency },
+  });
 };
 
 const findAllCurrencyFromWalletId = (id) => {
-    return Currency.findAll({ where: { wallet_id: id } })
+  return Currency.findAll({ where: { wallet_id: id } });
 };
 
 const deleteAccount = (id) => {
-    return true
-    // const transaction = sequelize.transaction()
-    // try {
-    //     Transaction.destroy({
-    //         where: { wallet_id: id },
-    //         transaction: transaction
-    //     })
+  return true;
+  // const transaction = sequelize.transaction()
+  // try {
+  //     Transaction.destroy({
+  //         where: { wallet_id: id },
+  //         transaction: transaction
+  //     })
 
-    //     Currency.destroy({
-    //         where: { wallet_id: id },
-    //         transaction: transaction
-    //     })
+  //     Currency.destroy({
+  //         where: { wallet_id: id },
+  //         transaction: transaction
+  //     })
 
-    //     Wallet.destroy({
-    //         where: { id: id },
-    //         transaction: transaction
-    //     })
+  //     Wallet.destroy({
+  //         where: { id: id },
+  //         transaction: transaction
+  //     })
 
-    //     transaction.commit();
-    //     return true
-    // } catch (error) {
-    //     // transaction.rollback()
-    //     return false
-    // }
+  //     transaction.commit();
+  //     return true
+  // } catch (error) {
+  //     // transaction.rollback()
+  //     return false
+  // }
 };
 
 module.exports = {
-    findUserByUsername,
-    findAllWalletById,
-    findExchangeRateByCountry,
-    findAllCurrencyFromWalletId,
-    deleteAccount,
-    findCurrencyFromWalletId
+  findUserByUsername,
+  findAllWalletById,
+  findExchangeRateByCountry,
+  findAllCurrencyFromWalletId,
+  deleteAccount,
+  findCurrencyFromWalletId,
 };
