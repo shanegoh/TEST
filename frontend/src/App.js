eebe02ae484fac4ade06375f88515efb6fe6eec9
Lim Xin Da <lim.xin.da@hotmail.com>
2022-07-22 17:52:43 +0800

    working login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,25 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Login from "./components/Login";
+//import "./App.css";
 import Dashboard from "./components/Dashboard";
-import "./App.css";
-import { DataController } from "./components/DataController";
-
-// export const API_URL = "https://api.exchangeratesapi.io/latest";
-/*
-  "https://api.exchangeratesapi.io/latest" doesn't work since 1st April 2021
-  and currently it returns:
-  {
-    "success": false,
-    "error": {
-      "code": 101,
-      "type": "missing_access_key",
-      "info": "You have not supplied an API Access Key. [Required format: access_key=YOUR_ACCESS_KEY]"
-    }
-  }
-*/
-
-const API_ACCESS_KEY = "YOUR_API_ACCESS_KEY";
-export const API_URL = `https://api.exchangeratesapi.io/v1/latest?access_key=${API_ACCESS_KEY}`;
+import { Container } from "react-bootstrap";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
-   <Login></Login>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "80vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "600px" }}>
+        {isAuthenticated ? (
+          <Dashboard setIsAuthenticated={setIsAuthenticated} />
+        ) : (
+          <Login setIsAuthenticated={setIsAuthenticated} />
+        )}
+      </div>
+    </Container>
   );
 }
 
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,60 +1,85 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React, { useState, useRef } from "react";
+import { Card, Form, Alert, Button } from "react-bootstrap";
+import axios from "axios";
 
 const Login = (props) => {
-    const [user, setUsername] = useState("");
-    const [pass, setPassword] = useState("");
-    const [loginError, setLoginError] = useState(false);
-    const submit = (e) => {
-        e.preventDefault();
-        var bodyFormData = new FormData();
-        bodyFormData.append('username', e.target[0].value);
-        bodyFormData.append('password', e.target[1].value);
-        axios({
-            method: "post",
-            url: "http://localhost:5000/api/login",
-            data: bodyFormData,
-            headers: { "Content-Type": "multipart/form-data" },
-        })
-        .then( (res)=> {
-            props.setIsAuthenticated(true);
-        }).catch((error)=> {
-            setLoginError(true);
-        });
-    }
-    return (
-        <div className="container">
-        <form onSubmit={submit}>
-            <div>
-                <label htmlFor="username">Username:</label>
-                <input type="text"
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const [user, setUsername] = useState("");
+  const [pass, setPassword] = useState("");
+  const [loginError, setLoginError] = useState(false);
+
+  const submit = (e) => {
+    e.preventDefault();
+    const bodyFormData = new FormData();
+
+    bodyFormData.append("username", userRef.current.value);
+    bodyFormData.append("password", passwordRef.current.value);
+
+    console.log(bodyFormData.getAll("password"));
+
+    axios({
+      method: "post",
+      url: "http://localhost:5000/api/login",
+      data: bodyFormData,
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+      .then((res) => {
+        props.setIsAuthenticated(true);
+        console.log("You have logged in");
+      })
+      .catch((error) => {
+        setLoginError(true);
+      });
+  };
+
+  return (
+    <>
+      <Card className="mx-auto" style={{ maxWidth: "400px" }}>
+        <Card.Body>
+          <h2 className="text-center mb-4">Log In</h2>
+          <Form onSubmit={submit}>
+            <Form.Group>
+              <Form.Label htmlFor="username">Username:</Form.Label>
+              <Form.Control
+                ref={userRef}
+                type="text"
                 autoComplete="off"
-                id = "username"
-                name = "username"
-                placeholder = "Enter your username here"
-                onChange = {(e)=>{
-                    setUsername(e.target.value)
-                }}/>
-            </div>
-            <div>
-                <label htmlFor="password">Password:</label>
-                <input type="password"
-                autoComplete="off"
-                id = "password"
-                name = "password"
-                placeholder = "Enter your password here"
-                onChange = {(e)=>{
-                    setPassword(e.target.value)
+                id="username"
+                name="username"
+                placeholder="Enter your username here"
+                onChange={(e) => {
+                  setUsername(e.target.value);
                 }}
-                />
-            </div>
-            {loginError && <div id = "errorMessage" className = "generic-error">
-                <p> The credentials you have entered are not valid.</p>
-            </div>}
-            <button type="submit">SUBMIT</button>
-        </form>
-        </div>
-    )
-}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="password">Password:</Form.Label>
+              <Form.Control
+                ref={passwordRef}
+                type="password"
+                autoComplete="off"
+                id="password"
+                name="password"
+                placeholder="Enter your password here"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </Form.Group>
+            {loginError && (
+              <Alert variant="danger" className="mt-2">
+                The credentials you have entered are not valid
+              </Alert>
+            )}
+            <Button type="submit" className="w-100 mt-2">
+              Log in
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 
 export default Login;
