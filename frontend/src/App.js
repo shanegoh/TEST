6f64b44712cf7abb9dc8fa26960ee86929bc7cd2
Yew Teck <yewteck.n@gmail.com>
2022-07-22 15:51:24 +0800

    bug fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
-import "App.css";
-import { DataController } from "components/DataController";
+import React, {useState} from "react";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import "./App.css";
+import { DataController } from "./components/DataController";
 
 // export const API_URL = "https://api.exchangeratesapi.io/latest";
 /*
@@ -20,13 +22,10 @@ const API_ACCESS_KEY = "YOUR_API_ACCESS_KEY";
 export const API_URL = `https://api.exchangeratesapi.io/v1/latest?access_key=${API_ACCESS_KEY}`;
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
-    <div className="app">
-      <header className="app-header">
-        Currency converter
-      </header>
-      <DataController url={API_URL} />
-    </div>
+   <Login></Login>
   );
 }
 
